8187a80f99f418d9bb8e605becf840a6dea9ea57
Robin Grimaud <robin.grimaud@etudiant.univ-reims.fr>
2015-11-27 15:30:32 +0100

    Commit du 27/11/2015 + base de données

---
+++ infs3_prj15.sql
@@ -0,0 +1,459 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.12
+-- http://www.phpmyadmin.net
+--
+-- Client :  mysql
+-- Généré le :  Ven 27 Novembre 2015 à 15:26
+-- Version du serveur :  5.5.32-MariaDB
+-- Version de PHP :  5.3.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `infs3_prj15`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `arbitre`
+--
+
+CREATE TABLE IF NOT EXISTS `arbitre` (
+  `numArbitre` int(11) NOT NULL,
+`idPers` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categorie`
+--
+
+CREATE TABLE IF NOT EXISTS `categorie` (
+  `idCtg` int(11) NOT NULL,
+  `idNiveau` int(11) NOT NULL,
+  `nomCtg` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `urlCtg` longtext CHARACTER SET utf8
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `categorie`
+--
+
+INSERT INTO `categorie` (`idCtg`, `idNiveau`, `nomCtg`, `urlCtg`) VALUES
+(1, 1, 'Accueil', '    <p> \r\nFerrars all spirits his imagine effects amongst neither. It bachelor cheerful of mistaken. Tore has sons put upon wife use bred seen. Its dissimilar invitation ten has discretion unreserved. Had you him humoured jointure ask expenses learning. Blush on in jokes sense do do. Brother hundred he assured reached on up no. On am nearer missed lovers. To it mother extent temper figure better.\r\nFerrars all spirits his imagine effects amongst neither. It bachelor cheerful of mistaken. Tore has sons put upon wife use bred seen. Its dissimilar invitation ten has discretion unreserved. Had you him humoured jointure ask expenses learning. Blush on in jokes sense do do. Brother hundred he assured reached on up no. On am nearer missed lovers. To it mother extent temper figure better. \r\n    </p>\r\n    <p> \r\nFerrars all spirits his imagine effects amongst neither. It bachelor cheerful of mistaken. Tore has sons put upon wife use bred seen. Its dissimilar invitation ten has discretion unreserved. Had you him humoured jointure ask expenses learning. Blush on in jokes sense do do. Brother hundred he assured reached on up no. On am nearer missed lovers. To it mother extent temper figure better.\r\nFerrars all spirits his imagine effects amongst neither. It bachelor cheerful of mistaken. Tore has sons put upon wife use bred seen. Its dissimilar invitation ten has discretion unreserved. Had you him humoured jointure ask expenses learning. Blush on in jokes sense do do. Brother hundred he assured reached on up no. On am nearer missed lovers. To it mother extent temper figure better. \r\n    </p> '),
+(2, 1, 'L''ASSBC', '<p> ASSBC </p>'),
+(3, 1, 'Equipes', '<p> Equipes </p>'),
+(4, 1, 'Classement et Résultats', '<p> Classement et Résultats </p>'),
+(5, 1, 'Photos', NULL),
+(6, 1, 'La salle', NULL),
+(7, 2, 'Documents', NULL),
+(8, 1, 'Evénements', NULL),
+(9, 1, 'Partenaires', NULL),
+(10, 1, 'Contact', NULL),
+(11, 3, 'Administration', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `championnat`
+--
+
+CREATE TABLE IF NOT EXISTS `championnat` (
+`idChampionnat` int(11) NOT NULL,
+  `nomChampionnat` varchar(20) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `equipe`
+--
+
+CREATE TABLE IF NOT EXISTS `equipe` (
+`idEquipe` int(11) NOT NULL,
+  `idChampionnat` int(11) NOT NULL,
+  `nomEquipe` varchar(15) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `evenement`
+--
+
+CREATE TABLE IF NOT EXISTS `evenement` (
+  `idEvenement` int(11) NOT NULL,
+  `idTypeEvenement` int(11) NOT NULL,
+  `dateEvenement` date NOT NULL,
+  `libEvenement` varchar(20) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `evenement`
+--
+
+INSERT INTO `evenement` (`idEvenement`, `idTypeEvenement`, `dateEvenement`, `libEvenement`) VALUES
+(1, 1, '2015-10-27', 'soirée de bienvenue');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `joueur`
+--
+
+CREATE TABLE IF NOT EXISTS `joueur` (
+  `postePrincipal` int(11) NOT NULL,
+  `idEquipe` int(11) NOT NULL,
+`idPers` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `match`
+--
+
+CREATE TABLE IF NOT EXISTS `match` (
+  `idMatch` int(10) NOT NULL,
+  `idEquipeDom` int(10) NOT NULL,
+  `idSalle` int(10) NOT NULL,
+  `idSaison` int(10) NOT NULL,
+  `dateMatch` date NOT NULL,
+  `noJournee` int(10) NOT NULL,
+  `idPers` int(10) NOT NULL,
+  `idEquipeVisiteur` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `menu`
+--
+
+CREATE TABLE IF NOT EXISTS `menu` (
+  `idMenu` int(11) NOT NULL,
+  `nomMenu` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `urlMenu` longtext CHARACTER SET utf8,
+  `idCtg` int(11) NOT NULL,
+  `idNiveau` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `menu`
+--
+
+INSERT INTO `menu` (`idMenu`, `nomMenu`, `urlMenu`, `idCtg`, `idNiveau`) VALUES
+(1, 'Organigramme', '<p>A l''issue de l''AG du 26 mai 2013, un nouveau bureau a été désigné.</p>\r\n<p>Voici le bureau de l''ASSBC Basket pour la saison 2013-2014 : </p>\r\n<p><span>Présidente</span> : Pauline MAISSIN</p>\r\n<p><span>Vice-présidente</span> : Dolyne GRONDIN</p>\r\n<p><span>Secrétaire</span> : Alexandra FAGNON</p>\r\n<p><span>Secrétaire adjointe</span> : Virginie DEFAUCHEUX (responsable licences)</p>\r\n<p><span>Trésorier</span> : Julien LAPLAIGE</p>\r\n<ul>\r\n	<li><span>Membres actifs</span> : Estelle CASANOVA</li>\r\n	<li>David TEIXEIRA</li>\r\n	<li>Guillaume CHILD</li>\r\n</ul>', 2, 1),
+(2, 'Comptes Rendus', '<p> Comptes Rendus </p>', 2, 2),
+(3, 'Un peu d''histoire ...', '<p>La première association <span style = "text-decoration : underline; font-weight : bold;">ASSBC</span> (<span style = "font-weight : bold;">A</span>ssociation <span style = "font-weight : bold;">S</span>portive <span style = "font-weight : bold;">S</span>aint <span style = "font-weight : bold;">B</span>rice <span style = "font-weight : bold;">C</span>ourcelles) a été déclarée en 1976 à la Préfecture de la Marne. Cette association était un regroupement des différentes sections (handball, football, basketball, ...) avec un président unique. </p>\r\n\r\n\r\n<p>Le créateur et premier président de l''<span style = "text-decoration : underline; font-weight : bold;">ASSBC Basket</span> était Monsieur HEBERT Maurice. \r\nL''association, sous sa forme actuelle, a été déclarée par Monsieur HEBERT en novembre 1995. </p>\r\n\r\n<p>En 1985, Monsieur LANCELEUX Jean-Luc est devenu secrétaire jusqu''en 1999, année de son élection à la présidence du club. Jean-Luc est resté président du club pendant 10 ans. </p>\r\n<p>En 2009, Monsieur Vincent CORDIER a été élu président. En 2011, il a passé la main à Madame Estelle CASANOVA jusqu''alors vice-présidente. En juin 2013, Madame Pauline MAISSIN a été élue présidente de l''association. </p>\r\n\r\n<p>À ses débuts, le club avait des équipes jeunes (garçons et filles) puis a grandi au fil du temps pour avoir une section masculine puis une équipe féminine au début des 90''. </p>\r\n<p>Pour la saison 2013-2014, le club a engagé 5 équipes en championnat (2 équipes masculines, 1 équipe féminine et 3 équipes jeunes).</p>\r\n\r\n<p>Le club était à son apogée au milieu des années 90 avec presque 100 licenciés. Pendant la saison 2012-2013, l''ASSBC Basket réunissait 69 licenciés. </p>', 2, 1),
+(4, 'L''esprit du club', '<p> L''esprit du club </p>', 2, 1),
+(5, 'Pré-National Masculin', NULL, 3, 1),
+(6, 'Pré-National Féminin', NULL, 3, 1),
+(7, 'Excellence Départementale Masculin', NULL, 3, 1),
+(8, 'Excellence Départementale Feminin', NULL, 3, 1),
+(9, 'U17 Masculin', NULL, 3, 1),
+(10, 'U15 Masculin', NULL, 3, 1),
+(11, 'U11 Masculin', NULL, 3, 1),
+(12, 'Ecole de basket (6-8 ans)', NULL, 3, 1),
+(13, 'Calendrier', NULL, 4, 1),
+(14, 'Résultats d''équipes', NULL, 4, 1),
+(15, 'Soirées', NULL, 8, 2),
+(16, 'Stages ', NULL, 8, 2),
+(17, 'Divers', NULL, 8, 1),
+(18, 'Tournois', NULL, 8, 1),
+(19, 'Gestion des menus', NULL, 11, 4),
+(20, 'Gestion des utilisateurs', NULL, 11, 4),
+(21, 'Mise à jour de la page d''accueil', NULL, 11, 3),
+(22, 'Ajout de comptes rendus de réunion', NULL, 11, 3),
+(23, 'Mise à jour des résultats d''équipes', NULL, 11, 3),
+(24, 'Gestion de l''album photo', NULL, 11, 3),
+(25, 'Gestion du calendrier', NULL, 11, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `niveau`
+--
+
+CREATE TABLE IF NOT EXISTS `niveau` (
+`idNiveau` int(10) NOT NULL,
+  `libNiveau` varchar(20) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `niveau`
+--
+
+INSERT INTO `niveau` (`idNiveau`, `libNiveau`) VALUES
+(1, 'Invité'),
+(2, 'Membre'),
+(3, 'Bureau'),
+(4, 'Administrateur');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `personne`
+--
+
+CREATE TABLE IF NOT EXISTS `personne` (
+`idPers` int(11) NOT NULL,
+  `nomPers` varchar(20) CHARACTER SET utf8 NOT NULL,
+  `prenomPers` varchar(20) CHARACTER SET utf8 NOT NULL,
+  `adressMailPers` varchar(40) CHARACTER SET utf8 NOT NULL,
+  `adressPers` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `villePers` varchar(40) CHARACTER SET utf8 NOT NULL,
+  `cpPers` varchar(5) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `personne`
+--
+
+INSERT INTO `personne` (`idPers`, `nomPers`, `prenomPers`, `adressMailPers`, `adressPers`, `villePers`, `cpPers`) VALUES
+(1, 'Grimaud', 'Robin', 'robin.grimaud@etudiant.univ-reims.fr', '2 rue du pont', 'Reims', '51100'),
+(2, 'Lallement', 'Jaufre', 'jaufre.lallement@etudiant.univ-reims.fr', '6 rue jean michel', 'reims', '51000'),
+(3, 'bureau', 'bureau', 'bureau.bureau@etudiant.univ-reims.fr', '10 rue du bureau', 'Bureau', '51000'),
+(4, 'membre', 'membre', 'membre.membre@etudiant.univ-reims.fr', '5 rue du membre', 'Membre', '51000'),
+(5, 'Jean', 'Dupont', 'jean.dupon@etudiant.univ-reims.fr', '5 rue du pc', 'reims', '51000'),
+(6, 'Isartelle', 'Patrick', 'patrick.isartelle@univ-reims.fr', '6 rue du moulin', 'charleville-mézières', '08000'),
+(7, 'dzazd', 'dazdazd', 'a.a@a.fr', 'a.a@a.fr', '', ''),
+(8, 'fddazdaz', 'dazdazd', 'a.a@htbt.fr', 'a.a@htbt.fr', '', ''),
+(9, 'az', 'az', 'a.a@htbt.fr', 'a.a@htbt.fr', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `photo`
+--
+
+CREATE TABLE IF NOT EXISTS `photo` (
+  `idPhoto` int(11) NOT NULL,
+  `libPhoto` varchar(20) CHARACTER SET utf8 NOT NULL,
+  `legende` text CHARACTER SET utf8 NOT NULL,
+  `image` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `saison`
+--
+
+CREATE TABLE IF NOT EXISTS `saison` (
+`idSaison` int(11) NOT NULL,
+  `idChampionnat` int(11) NOT NULL,
+  `libSaison` varchar(20) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `salle`
+--
+
+CREATE TABLE IF NOT EXISTS `salle` (
+  `idSalle` int(10) NOT NULL,
+  `libSalle` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `villeSalle` varchar(40) CHARACTER SET utf8 NOT NULL,
+  `rueSalle` text CHARACTER SET utf8 NOT NULL,
+  `cpSalle` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `typeevenement`
+--
+
+CREATE TABLE IF NOT EXISTS `typeevenement` (
+  `idTypeEvenement` int(11) NOT NULL,
+  `libTypeEvenement` varchar(20) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `typeevenement`
+--
+
+INSERT INTO `typeevenement` (`idTypeEvenement`, `libTypeEvenement`) VALUES
+(1, 'Soirée');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `commentaire` text NOT NULL,
+  `login` varchar(20) NOT NULL,
+  `idNiveau` int(11) NOT NULL,
+`idPers` int(11) NOT NULL,
+  `mdp` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`commentaire`, `login`, `idNiveau`, `idPers`, `mdp`) VALUES
+('c''est robin !!', 'grima001', 4, 1, '84fa5fc48ae301cd6808342dadf498d86a7dfd66'),
+('c''est jaufre !!', 'lalle001', 4, 2, '02553c7faada6f0cd3ae19d0ade3351027aa6af2'),
+('c''est bureau !!', 'burea001', 3, 3, '688c24ec15901a9da7041a2f497c93ecff3095e7'),
+('c''est membre !!', 'membr001', 2, 4, '587679f84349e8bf34281af770b1e6e3882d9e67'),
+('c''est jean !!', 'dupon001', 2, 5, '0792bdb7b7c8fb1e51b1a203af7ea120b3d63ec4'),
+('c''est patrick !!', 'isart001', 2, 6, 'adbaf3223048a5b125c62152ec2d4312351c799d'),
+('', 'dazd', 2, 7, 'de5eaf0bfb7796ff84e97c916d7d99c440062b8b'),
+('', 'dazdaz', 2, 8, '37c2e165ef60fb6d7ea090e79c3dd93dc78f02cd'),
+('', 'az', 2, 9, 'f54a8e725fdcd4b0b5e4f05604fd24358cd70c8f');
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `arbitre`
+--
+ALTER TABLE `arbitre`
+ ADD PRIMARY KEY (`idPers`);
+
+--
+-- Index pour la table `categorie`
+--
+ALTER TABLE `categorie`
+ ADD PRIMARY KEY (`idCtg`);
+
+--
+-- Index pour la table `championnat`
+--
+ALTER TABLE `championnat`
+ ADD PRIMARY KEY (`idChampionnat`);
+
+--
+-- Index pour la table `equipe`
+--
+ALTER TABLE `equipe`
+ ADD PRIMARY KEY (`idEquipe`);
+
+--
+-- Index pour la table `evenement`
+--
+ALTER TABLE `evenement`
+ ADD PRIMARY KEY (`idEvenement`);
+
+--
+-- Index pour la table `joueur`
+--
+ALTER TABLE `joueur`
+ ADD PRIMARY KEY (`idPers`);
+
+--
+-- Index pour la table `match`
+--
+ALTER TABLE `match`
+ ADD PRIMARY KEY (`idMatch`);
+
+--
+-- Index pour la table `menu`
+--
+ALTER TABLE `menu`
+ ADD PRIMARY KEY (`idMenu`);
+
+--
+-- Index pour la table `niveau`
+--
+ALTER TABLE `niveau`
+ ADD PRIMARY KEY (`idNiveau`);
+
+--
+-- Index pour la table `personne`
+--
+ALTER TABLE `personne`
+ ADD PRIMARY KEY (`idPers`);
+
+--
+-- Index pour la table `photo`
+--
+ALTER TABLE `photo`
+ ADD PRIMARY KEY (`idPhoto`);
+
+--
+-- Index pour la table `saison`
+--
+ALTER TABLE `saison`
+ ADD PRIMARY KEY (`idSaison`);
+
+--
+-- Index pour la table `salle`
+--
+ALTER TABLE `salle`
+ ADD PRIMARY KEY (`idSalle`);
+
+--
+-- Index pour la table `typeevenement`
+--
+ALTER TABLE `typeevenement`
+ ADD PRIMARY KEY (`idTypeEvenement`);
+
+--
+-- Index pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+ ADD PRIMARY KEY (`idPers`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `arbitre`
+--
+ALTER TABLE `arbitre`
+MODIFY `idPers` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `championnat`
+--
+ALTER TABLE `championnat`
+MODIFY `idChampionnat` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `equipe`
+--
+ALTER TABLE `equipe`
+MODIFY `idEquipe` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `joueur`
+--
+ALTER TABLE `joueur`
+MODIFY `idPers` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `niveau`
+--
+ALTER TABLE `niveau`
+MODIFY `idNiveau` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT pour la table `personne`
+--
+ALTER TABLE `personne`
+MODIFY `idPers` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT pour la table `saison`
+--
+ALTER TABLE `saison`
+MODIFY `idSaison` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+MODIFY `idPers` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
